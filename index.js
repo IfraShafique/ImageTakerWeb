e51ec08ffb01d2c914db635a1336cf68644060d2
Ifra Shafique <86965873+IfraShafique@users.noreply.github.com>
2023-08-05 03:56:24 +0500

    New Update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let currentSlideID = 1;
     const sliderElement = document.getElementById('slider');
+    const sliderElement1 = document.getElementById('slider1');
     const totalSlides = sliderElement.childElementCount;
+    const totalSlides1 = sliderElement1.childElementCount;
 
     function next() {
       currentSlideID++;
@@ -8,6 +10,14 @@ let currentSlideID = 1;
         currentSlideID = 1;
       }
       showSlide();
+    }
+
+    function next1() {
+      currentSlideID++;
+      if (currentSlideID > totalSlides1) {
+        currentSlideID = 1;
+      }
+      showSlide1();
     }
 
     function showSlide() {
@@ -22,5 +32,18 @@ let currentSlideID = 1;
       }
     }
 
+    function showSlide1() {
+      const slides1 = sliderElement1.getElementsByTagName('li');
+      for (let i = 0; i < totalSlides1; i++) {
+        const element1 = slides1[i];
+        if (currentSlideID === i + 1) {
+          element1.classList.remove('hidden');
+        } else {
+          element1.classList.add('hidden');
+        }
+      }
+    }
+
     // Show the first slide initially
     showSlide();
+    showSlide1();
